0f2843e7c24db8e71957f4843e7e924a57b2d669
Kevin See <kevsee@gmail.com>
2026-07-09 09:04:52 -0700

    small updates to PNAMP presentation

--- presentation/render_pdf.R
+++ presentation/render_pdf.R
@@ -1,31 +1,47 @@
 # Author: Kevin See
 # Purpose: turn html presentation into PDF
-# Created: 110/2024
-# Last Modified: 1/10/2024
+# Created: 1/10/2024
+# Last Modified: 4/30/25
 # Notes:
 
 #-----------------------------------------------------------------
+library(here)
+
+# what file to render?
+# file_nm = "PITcleanr_Presentation"
+file_nm = "PITcleanr_Presentation_PNAMP"
+
+
+#-----------------------------------------------------------------
 # load needed libraries
+library(pagedown)
+
+chrome_print(input = here("presentation",
+                          paste0(file_nm, ".html")),
+             format = "pdf")
+
+#-----------------------------------------------------------------
+# try a different package
 library(renderthis)
+# renderthis package no longer supported by CRAN, but is available on GitHub
+# https://github.com/jhelvy/renderthis
 
 # render presentation into PDF
-to_pdf(from = "rmd/PITcleanr_Presentation.html",
-       to = "rmd/PITcleanr_Presentation.pdf",
+to_pdf(from = here("presentation",
+                   paste0(file_nm, ".html")),
+       to = here("presentation",
+                 paste0(file_nm, ".pdf")),
        complex_slides = T,
        partial_slides = T)
 
 # render presentation into powerpoint
-to_pptx(from = "rmd/PITcleanr_Presentation.html",
-        to = "rmd/PITcleanr_Presentation.pptx",
+to_pptx(from = here("presentation",
+                    paste0(file_nm, ".html")),
+        to = here("presentation",
+                  paste0(file_nm, ".pptx")),
         density = 300,
         complex_slides = T,
         partial_slides = T,
         delay = F,
         ratio = "16:9")
 
-#-----------------------------------------------------------------
-# try a different package
-library(pagedown)
-
-chrome_print(input = "rmd/PITcleanr_Presentation.html",
-             format = "pdf")
